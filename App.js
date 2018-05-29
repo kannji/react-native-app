a5dc9a74dad82654adf20d35b141c7c80b06f3f0
Jan-Luca Klees <email@janlucaklees.de>
2018-05-29 21:42:27 +0200

    installed react-native-elements

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up App.j working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <FormLabel>Name</FormLabel>
+        <FormInput />
+        <FormValidationMessage>Error message</FormValidationMessage>
       </View>
     );
   }
