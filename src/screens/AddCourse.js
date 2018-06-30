ce70d1c6037fe1525c11363026b268b70279be98
Jan-Luca Klees <email@janlucaklees.de>
2018-06-30 22:44:46 +0200

    Made AddCourse screen adapt to ne ES / CQRS model
    With this commit, the AddCourse screen will no longer directly persist
    the course, but will instead trigger the event indicating the creation
    of a new Course.

--- src/screens/AddCourse.js
+++ src/screens/AddCourse.js
@@ -3,6 +3,9 @@ import { StyleSheet, TextInput, Text, View} from 'react-native';
 import { Header, Icon, FormLabel, FormInput, FormValidationMessage, Button } from 'react-native-elements';
 
 import * as db from '../db';
+import EventBus from '../events/EventBus';
+import CourseCreated from '../events/CourseCreated';
+import CoursePersisted from '../events/CoursePersisted';
 
 export default class CreateCourse extends React.PureComponent {
 
@@ -19,15 +22,17 @@ export default class CreateCourse extends React.PureComponent {
         this.setState({
             isAdding: true
         });
+        
+        let createdCourseEvent = new CourseCreated({
+            name: this.state.name,
+            description: this.state.description
+        }, EventBus.generateNewStreamId());
 
-        db.addCourse({
-            newCourse: {
-                name: this.state.name,
-                description: this.state.description
-            }
-        }).then(() => {
+        createdCourseEvent.onReaction( CoursePersisted, ( persistedCourseEvent ) => {
             this.props.navigation.navigate('Home');
         });
+
+        createdCourseEvent.trigger( );
     }
 
     getButton() {
